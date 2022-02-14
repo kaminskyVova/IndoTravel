4e8bd9cd09c04c733270f49f7a7a41b73e1d786c
Kaminsky <kaminskyfrontend@gmail.com>
2022-02-14 14:57:26 +0200

    svg+burger

--- script/main.js
+++ script/main.js
@@ -1,6 +1,7 @@
 import {countdownTimer} from './modules/timer.js';
 import './modules/acc.js'
 import './modules/burgerMenu.js'
+import './modules/flay.js'
 
 const start = () => {
 
--- script/modules/burgerMenu.js
+++ script/modules/burgerMenu.js
@@ -1,19 +1,27 @@
 const body = document.querySelector('body');
 const menuList = body.querySelector('.header__menu');
+const menuBtn = body.querySelector('.nav-icon__middle');
+const menuBtnWrapper = body.querySelector('.nav-btn__wrapper');
+
+menuBtnWrapper.style.cssText = `
+width: 35px;
+height: 30px;
+`;
 
 body.addEventListener('click', ({ target }) => {
   console.log('target: ', target);
-  target.matches('.header__menu-button')
-    ? menuList.classList.toggle('header__menu_active')
-    : '';
+  if (target.closest('.nav-btn__wrapper')) {
+    menuList.classList.toggle('header__menu_active');
+    menuBtn.classList.toggle('active');
+  }
 
-	if (
-    target.classList.contains('header__menu') ||
+  if (
     target.classList.contains('header__link') ||
-    (target.className != 'header__menu' &&
-      !target.matches('.header__menu-button'))
+    (!target.classList.contains('header__menu_active') && !target.closest('.nav-btn__wrapper'))
   ) {
     menuList.classList.remove('header__menu_active');
+    menuBtn.classList.remove('active');
   }
 
 });
+
